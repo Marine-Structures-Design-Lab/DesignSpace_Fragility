fe356cb4137e88b367978976d36489fce5582dc5
joeyvan20 <joeyvan@umich.edu>
2023-02-02 19:00:43 -0500

    Frag_Framework planning
    Continuing to plan out exactly how the fragility framework, and the stuff around the fragility framework, will work with lots of chronological comments and indentations

--- Python Code/Frag_Frmwk.py
+++ Python Code/Frag_Frmwk.py
@@ -4,7 +4,7 @@ Created on Tue Jan 24 18:28:57 2023
 
 @author: Joseph B. Van Houten (joeyvan@umich.edu)
 
-This is going to act as a script file for the assessment of design space
+This is going to act as a script(?) file for the assessment of design space
 fragility.  For now, this script file is only going to focus on comparing the
 fragility of a design space for any proposed space reduction to the fragility
 of the design space without that reduction to quantify reduction risk, so
@@ -14,8 +14,7 @@ to try to determine what exactly is a high-risk reduction.  Nor does the
 program need to focus on guiding any further exploration or condensing of
 the size of the proposed reduction to alleviate some of that risk.
 
-
-[Further Description]
+[Further Description/Revision of what is written above]
 """
 
 """
@@ -49,47 +48,11 @@ ELEMENTS OUTSIDE OF SCRIPT FILE:
 """
 
 """
-CHRONOLOGICAL COMMANDS OF SCRIPT FILE:
-    
-1) Calculate entropy levels of each discipline's design space without any
-   space reductions
-    - What entropy types?
-    - How should the entropy characterize the space?
-        -- A single all-encompassing entropy value for each space
-        -- Entropy contour map
-        -- Entropy value assigned to each individual point based on what is
-           around it (perceived boundaries and other points)
-
-2) Introduce perturubations
-    - Potentially move points to other test points?
-    - Potentially more points to perceived feasible or dominated boundaries?
-    - Should start simple to test out the entropy metrics by just randomly
-      moving a boundary and/or randomly moving each point a specified distance
-      away from its current location
-      
-3) Calculate new entropy level's of each discipline's design space without any
-   space reductions
-   
-4) Calculate difference between new and original entropy levels of each
-   discipline
-   
-5) Introduce the space reduction(s)
-    - One at a time or all at once or some other sort of combination?
-    
-6) Repeat steps 1-4 with the reduced design space
-    
-7) Compare the entropy level differences before of step 4 before and after
-   introducing the proposed space reduction(s)
-
-"""
-
-"""
 WHERE TO FOCUS RESEARCH ATTENTION:
 
 1) How to use entropy to calculate uncertainty associated with perceptions of a
    sampled design space
 
-
 """
 
 
@@ -100,15 +63,22 @@ WHERE TO FOCUS RESEARCH ATTENTION:
 
 
 """
-SCRIPT
+COMMANDS
 """
 
 # -------------Automated Exploration & Space Reduction Proposition-------------
 
 # ALL OF THE CODE FOR ANALYZING DATA AND PROPOSING REDUCTIONS BEFORE
-# INCORPORATING ANY SORT OF A FRAGILITY FRAMEWORK --> This section and the last
-# section will actually probably be better off in a "Script" file that is bound
-# to call the fragility framework...a level up so to say
+# INCORPORATING ANY SORT OF A FRAGILITY FRAMEWORK --> This section will
+# actually probably be better off in a "Script" file that is bound to call the
+# fragility framework file...a level up so to say
+
+
+
+
+
+
+
 
 
 
@@ -139,7 +109,8 @@ SCRIPT
     # continue to the next discipline of the for loop
     
     # Calculate the entropy of the particular discipline's remaining design
-    # space before factoring in any of its proposed reductions
+    # space before factoring in any proposed reductions --> Test out different
+    # function call(s) within an entropy-calculating class
     ### 1) Need to figure out exactly how the entropy is going to look.  I will
     ###    likely start out with some sort of Shannon Entropy, but I need to
     ###    determine exactly how that gets calculated for a design space having
@@ -170,15 +141,153 @@ SCRIPT
     ###          design point of the particular discipline is passing/failing
     
     # Perturb the data of the particular discipline's remaining design space
-    # before factoring in any of its proposed reductions
+    # before factoring in any of its proposed reductions --> Test out different
+    # function call(s) within a perturbation class
+    ### There is a spectrum of ideas on how perturbations could be introduced.
+    ### The simplest strategy would be to create move boundaries and points a
+    ### set distance away and/or introduce new requirements/points based on
+    ### what would be reasonable for the specific design problem.  But this
+    ### requires the designer to create an all-encompassing list of possible
+    ### design changes that may occur when running the fragility framework.  A
+    ### more reasonable strategy may be to suppose worst-case scenarios or
+    ### scenarios of points possibly moving to other points or perceived bounds
+    ### in their immediate vicinity and observing perhaps how the distances
+    ### differ between the output and input spaces for those moves.  Another
+    ### strategy may be to suppose that each point has a bubble around it of
+    ### possible locations to where the points could change, and then to
+    ### observe how that bubble fits in together with all of the bubbles of the
+    ### other surrounding points.  This is likely the kind of direction the
+    ### perturbations aspect of the fragility framework will take, but for now,
+    ### it will likely be easiest and straightforward to test out random
+    ### shifts in points and boundaries while we get a grasp on how the entropy
+    ### metrics and characterization of the space will look.
     
+    # Calculate the entropy of the particular discipline's remaining design
+    # space before factoring in any of its proposed reductions but after
+    # introducing the perturbations --> Test out different function call(s)
+    # within the sample entropy-calculating class
+    
+    # Determine the difference in the particular discipline's entropy levels
+    # before introducing the perturbations as compared to after introducing
+    # the perturbations --> Test out different function call(s) within an
+    # entropy-comparing class
+    ### This may be an interesting step and may be dependent on exactly how
+    ### entropy is used to classify a design space.  If a design space is given
+    ### one value that describes the entire space, it could be as simple as one
+    ### difference calculation.  However, if an entropy contour map is used, or
+    ### if each test point is given an entropy value to hold onto but then all
+    ### of those points move, this difference "calculation" will become more
+    ### involved.
+    
+# Stop looping through each discipline of the problem
+
+# Loop through each possible combination of space reductions in the list - 
+# excluding the possible combination of no space reductions considered because
+# there would be no fragility comparison to be made for that case
+### This part of the code may involve some sort of decision to make it more
+### concise.  Let's say 4 reductions are proposed at a particular moment in
+### time.  While eventually, the decision may be made to just assess the
+### resulting fragility that each of these proposed reductions have on a design
+### space individually or as a whole, it makes sense to also test out all of
+### the possible intermediate combinations of these reductions.  For example,
+### maybe we find out that when looking at each reduction individually, those
+### reductions do not have a huge affect on the fragility of any of the design
+### spaces.  But when we look at them altogether, we see a huge impact on the
+### fragility of each, or particular, design spaces.  This may make us want to
+### break down the possible combinations further.  There may be a noticeable
+### difference in the impact on a discipline's fragility when the 1st and 2nd
+### proposed reductions are looked at together compared to when the 1st and 4th
+### reductions are looked at together.  Until we have gathered some entropic
+### data from this, the best option is likely to look at every possible
+### combination of space reductions proposed at a particular moment in time.
+### If 4 reductions are proposed at that particular moment, this would mean
+### having 15 different combinations to look at (where combinations are
+### different than permutations because order does not matter, and there is no
+### set length of the number of reductions that have to be in a combination).
+### Those combinations would look as such - 1, 2, 3, 4, 1&2, 1&3, 1&4, 2&3,
+### 2&4, 3&4, 1&2&3, 1&2&4, 1&3&4, 2&3&4, 1&2&3&4.
+
+    # Reflect on the discipline(s) affected by the particular combination of
+    # space reductions and compile into a list
+    
+    # Loop through each discipline affected by the particular combination of
+    # space reductions in the list
+    ### All of the commands in this for loop should be done in the same manner
+    ### as they were done above for consistency of results
+    
+        # Calculate the entropy of the particular discipline's remaining design
+        # space after factoring in all proposed reductions of the particular
+        # combination
+        
+        # Perturb the data of the particular discipline's remaining design
+        # space after factoring in all proposed reductions of the particular
+        # combination
+        
+        # Calculate the entropy of the particular discipline's remaining design
+        # space after factoring in all proposed reductions of the particular
+        # combination and after introducing the perturbations
+        
+        # Determine the difference in the particular discipline's entropy
+        # levels before introducing the perturbations as compared to after
+        # introducing the perturbations
+    
+    # Stop looking through each discipline affected by the particular
+    # combination of space reductions in the list
+
+# Stop looping through each possible combination of space reductions in the
+# list
 
 
 
 
 # -------Comparison of Design Results with & without Fragility Framework-------
 
+# ALL OF THE CODE FOR ANALYZING HOW THE DESIGN SPACES WITHOUT ANY OF THE
+# PROPOSED SPACE REDUCTIONS HANDLE THE PERTURBATIONS COMPARED TO HOW THE DESIGN
+# SPACES WITH EACH POSSIBLE COMBINATION OF THE PROPOSED SPACE REDUCTIONS HANDLE
+# THE PERTURBATIONS TO DETERMINE HOW FRAGILE EACH OF THOSE POSSIBLE
+# COMBINATIONS OF SPACE REDUCTIONS WOULD END UP MAKING EACH DESIGN SPACE -->
+# This section may also be better off in its own "Script" file, taking the
+# results for the proposed reductions with the fragility framework and creating
+# observations tables and graphs for the proposed reductions at this particular
+# moment in time.  Could actually quantify the risk of reduction in this
+# section.
 
+
+# -------Next Steps for Design Manager to Make Based on Risk Assessment--------
+
+# Compare the risk to the project time remaining to either suggest not making
+# the proposed reduction at that point in time (step 1), condensing the
+# reduction to reduce the risk (step 2), and guiding where further exploration
+# should take place to gather more information before committing to the
+# reduction (step 3)
+
+
+
+
+
+# -------------Automated Exploration & Space Reduction Continuation------------
+
+# JUMP BACK INTO THE SCRIPT FILE THAT IS USED TO ANALYZE THE SET-BASED DESIGN
+# PROBLEM OF CONCERN, EXPLORE THE SPACE, AND PROPOSE REDUCTIONS.  EVENTUALLY
+# THIS SCRIPT WILL BE USED TO TEST A SBD PROCESS WITH THE FRAGILITY FRAMEWORK
+# AGAINST ONE THAT DOES NOT USE IT, AND WE WILL OBSERVE HOW EFFECTIVE A SBD
+# PROCESS IS WITH THE FRAMEWORK COMPARED TO WITHOUT THE FRAMEWORK.  AS THE
+# EXPLORATION AND REDUCTION PROCESS CONTINUES, WE WILL CAN HAVE EXPERIMENTS OF
+# THE FRAMEWORK WHEN IT PLANS FOR POSSIBLE DESIGN CHANGES BUT NO DESIGN CHANGES
+# ACTUALLY OCCUR, AS WELL AS EXPERIMENTS THAT ACTUALLY INTRODUCE RANDOM DESIGN
+# CHANGES AND RUN A BUNCH OF MONTE CARLO BASED EXPERIMENTS WITH THESE RANDOM
+# DESIGN CHANGES THAT POP UP
+
+
+
+
+
+
+
+# --How can all of this be similarly applied to an Iterative Design Process?---
+
+# See design notebook dated 1/27/23 for current notes/ideas on this...
 
 
 
