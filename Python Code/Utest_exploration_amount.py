32778ca8cec20fd9a3682c90a996f97c4e58d7ec
joeyvan20 <joeyvan@umich.edu>
2023-03-27 10:42:08 -0400

    Complete Unit test for exploration_amount

--- Python Code/Utest_exploration_amount.py
+++ Python Code/Utest_exploration_amount.py
@@ -1,29 +1,56 @@
 """
-Created on Thu Mar 23 13:28:22 2023
+SUMMARY:
+Unit tests for the exploreSpace class from exploration_amount.py
 
-@author: joeyv
+CREATOR:
+Joseph B. Van Houten
+joeyvan@umich.edu
 """
 
 """
 LIBRARIES
 """
+from exploration_amount import exploreSpace
 import unittest
-from exploration_amount import exploreSpace as expoS
 
 """
 CLASSES
 """
-class test_exploration_amount(unittest.TestCase):
-    
-    def setUp(self):
-        
-        ind = expoS.exploreSpace(0,100,[2,3,4])
+class test_exploration_amount(unittest.TestCase):      
     
     def test_fixed_explore(self):
-        
+        """
+        Unit tests for the fixedExplore method
+        """
+        ind = exploreSpace(0,100,[2,3,4])
         self.assertEqual(ind.fixedExplore(),20)
+        ind = exploreSpace(20,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),14)
+        ind = exploreSpace(35,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),9)
+        ind = exploreSpace(50,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),6)
+        ind = exploreSpace(60,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),4)
+        ind = exploreSpace(70,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),4)
+        ind = exploreSpace(80,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),4)
+        ind = exploreSpace(90,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),4)
+        ind = exploreSpace(96,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),4)
+        ind = exploreSpace(97,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),3)
+        ind = exploreSpace(98,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),2)
+        ind = exploreSpace(99,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),1)
+        ind = exploreSpace(100,100,[2,3,4])
+        self.assertEqual(ind.fixedExplore(),0)
+    
+    # Other methods
         
-
 """
 SCRIPT
 """
