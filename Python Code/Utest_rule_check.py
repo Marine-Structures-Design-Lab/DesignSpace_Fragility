3f357f3f87fa3be411bb41c0fe3c5bd6e6381804
joeyvan20 <joeyvan@umich.edu>
2023-03-28 18:14:23 -0400

    Unit Test for rule_check
    Completed the unit test

--- Python Code/Utest_rule_check.py
+++ Python Code/Utest_rule_check.py
@@ -1,7 +1,47 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Mar 28 16:40:25 2023
+SUMMARY:
+Unit tests for the ruleCheck function from rule_check.py
 
-@author: joeyv
+CREATOR:
+Joseph B. Van Houten
+joeyvan@umich.edu
 """
 
+"""
+LIBRARIES
+"""
+from rule_check import ruleCheck
+import unittest
+
+"""
+CLASSES
+"""
+class test_rule_check(unittest.TestCase):
+    
+    def setUp(self):
+        """
+        Established nested boolean lists for testing
+        """
+        self.list1 = [[[True,True],[True]],[[True,True]]]
+        self.list2 = [[[False,False],[True]],[[True,True]]]
+        self.list3 = [[[True,True],[False]],[[True,True]]]
+        self.list4 = [[[False,True],[False]],[[True,True]]]
+        self.list5 = [[[True,True],[True]],[[True,False]]]
+        self.list6 = [[[False,False],[False]],[[False,False]]]
+        
+    def test_rule_check(self):
+        """
+        Unit test for the ruleCheck function
+        """
+        self.assertTrue(ruleCheck(self.list1))
+        self.assertTrue(ruleCheck(self.list2))
+        self.assertTrue(ruleCheck(self.list3))
+        self.assertFalse(ruleCheck(self.list4))
+        self.assertFalse(ruleCheck(self.list5))
+        self.assertFalse(ruleCheck(self.list6))
+           
+"""
+SCRIPT
+"""
+suite = unittest.TestLoader().loadTestsFromTestCase(test_rule_check)
+unittest.TextTestRunner(verbosity=2).run(suite)
