43e393c8eaf858835147c48ac14373011650fe70
joeyvan20 <joeyvan@umich.edu>
2023-06-14 18:52:20 -0400

    New run_test

--- run_Utest.py
+++ run_Utest.py
@@ -10,10 +10,12 @@ joeyvan@umich.edu
 LIBRARIES
 """
 import os
+import requests
 import subprocess
+from datetime import datetime
 
 """
-SCRIPT
+SCRIPT FOR RUNNING TESTS
 """
 # Get the current working directory
 directory = os.getcwd()
@@ -37,12 +39,49 @@ for test_file in test_files:
     results[test_file] = {'status': status, 'output': result.stdout + '\n' + \
                           result.stderr}
 
-# Save the results to a text file
-# This will save the results in the same directory as this script
-with open('test_results.txt', 'w') as f:
-    for test_file, result in results.items():
-        f.write(
-            f'Results for {test_file} '
-            f'({result["status"]}): \n'
-            f'{result["output"]}\n\n'
-        )
+"""
+SCRIPT FOR SENDING EMAIL
+"""
+# Sendinblue API details
+api_key = os.environ['API_KEY']
+sender_email = os.environ['SENDER_EMAIL']
+recipient_email = os.environ['RECIPIENT_EMAIL']
+
+# Email details
+subject = 'Unit Testing Results'
+current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# Prepare the results content
+text_content = f'Here are the results of all the unit test files as of {current_datetime}:\n\n'
+for test_file, result in results.items():
+    text_content += (
+        f'Results for {test_file} '
+        f'({result["status"]}): \n'
+        f'{result["output"]}\n\n'
+    )
+
+# Prepare the data for the API request
+data = {
+    'sender': {
+        'name': sender_email,
+        'email': sender_email
+    },
+    'to': [
+        {
+            'email': recipient_email
+        }
+    ],
+    'subject': subject,
+    'textContent': text_content,
+}
+
+# Send the email using Sendinblue API
+headers = {'api-key': api_key}
+response = requests.post('https://api.sendinblue.com/v3/smtp/email', json=data, headers=headers)
+
+# Check the response status
+if response.status_code == 201:
+    print('Email sent successfully!')
+else:
+    print('Failed to send email. Status code:', response.status_code)
+    print('Response:', response.text)
