f395c42bfaba68f7d2fe77c1fd9d6a118bdde2cd
joeyvan20 <joeyvan@umich.edu>
2023-08-14 12:33:33 -0600

    Fragility_check update
    Updating methods in fragility check to calculate KL divergence for all combinations of input variables

--- Python Code/fragility_check.py
+++ Python Code/fragility_check.py
@@ -96,67 +96,65 @@ class checkFragility:
         return all_data
     
     
-    # (SHOULD BE ABLE TO COMEBACK TO THIS AND MAKE CODE LESS REPETITIVE)
+    # (SHOULD BE ABLE TO COME BACK TO THIS AND MAKE CODE LESS REPETITIVE)
     def calcKDEs(self, all_data, KLgap):
         
         # Initialize empty lists
+        joint_KDEs = [{} for _ in range(len(self.D))]
         KDEs = [{} for _ in range(len(self.D))]
-        joint_KDEs = [[] for _ in range(len(self.D))]
         
         # Loop through each discipline
         for i in range(0, len(self.D)):
             
-            # Determine the number of dimensions for joint KDE
+            # Determine the maximum number of dimensions for joint KDE
             num_dimensions = len(max(all_data[i], key=lambda k: len(k))) + 1
             
             # Loop through each variable combination
             for combo in all_data[i]:
                 
-                # Skip combo if the tuple is not a single input variable
-                if len(combo) > 1:
-                    continue
+                # Add combo to joint KDE dictionaries
+                joint_KDEs[i].setdefault(combo, [])
                 
-                # Add combo to KDE dictionaries if it does not already exist
-                KDEs[i].setdefault(combo, [])
+                # Check if tuple only consists of a single input variable
+                if len(combo) == 1:
+                    
+                    # Add combo to KDE dictionaries
+                    KDEs[i].setdefault(combo, [])
                 
                 # Establish minimum number of data points for KDE development
-                # (MIGHT GET A WARNING HERE IF I HAVE TOO LITTLE EXPLORED DATA)
-                if len(KDEs[i][combo]) == 0: min_data = num_dimensions + 1
-                else: min_data = len(KDEs[i][combo])*KLgap + num_dimensions + 1
+                # (MIGHT GET AN ERROR HERE IF I HAVE TOO LITTLE EXPLORED DATA)
+                if len(joint_KDEs[i][combo]) == 0: min_data = num_dimensions + 1
+                else: min_data = len(joint_KDEs[i][combo])*KLgap + num_dimensions + 1
                 
                 # Loop through data for KDE development
                 for j in range(min_data, all_data[i][combo].shape[0], KLgap):
                     
-                    # Gather data for particular combination
-                    combo_data = all_data[i][combo][0:j, 0:-1]
+                    # Gather data for joint combination
+                    joint_data = all_data[i][combo][0:j, :]
                     
-                    # Perform univariate KDE (free to adjust these!)
-                    bandwidth_univariate = 'scott'
-                    kernel_univariate = 'gau'
-                    kde = sm.nonparametric.KDEUnivariate(combo_data)
-                    kde.fit(bw=bandwidth_univariate, kernel=kernel_univariate)
+                    # Perform multivariate KDE (free to adjust these!)
+                    var_type_string = 'c' * (len(combo) + 1)
+                    bandwidth_multivariate = 'normal_reference'
+                    kde = sm.nonparametric.KDEMultivariate(data=joint_data, var_type=var_type_string, bw=bandwidth_multivariate)
                     
-                    # Append KDE to the combo's list
-                    KDEs[i][combo].append(kde)
-            
-            # Establish minimum number of data points for KDE development
-            if len(joint_KDEs[i]) == 0: min_data = min_data = num_dimensions + 1
-            else: min_data = len(joint_KDEs[i])*KLgap + num_dimensions + 1
-            
-            # Loop through data for KDE development
-            for j in range(min_data, all_data[i][combo].shape[0], KLgap):
-                
-                # Gather data for joint combination
-                joint_data = all_data[i][max(all_data[i], key=lambda k: len(k))][0:j, :]
-                
-                # Perform multivariate KDE (free to adjust these!)
-                var_type_string = 'c' * num_dimensions
-                bandwidth_multivariate = 'normal_reference'
-                kde = sm.nonparametric.KDEMultivariate(data=joint_data, var_type=var_type_string, bw=bandwidth_multivariate)
-                
-                # Append KDE to the joint KDE's list
-                joint_KDEs[i].append(kde)
-            
+                    # Append KDE to the joint KDE's list
+                    joint_KDEs[i][combo].append(kde)
+                    
+                    # Check if tuple only consists of a single input variable
+                    if len(combo) == 1:
+                        
+                        # Gather data for particular combination
+                        combo_data = all_data[i][combo][0:j, 0:-1]
+                        
+                        # Perform univariate KDE (free to adjust these!)
+                        bandwidth_univariate = 'scott'
+                        kernel_univariate = 'gau'
+                        kde = sm.nonparametric.KDEUnivariate(combo_data)
+                        kde.fit(bw=bandwidth_univariate, kernel=kernel_univariate)
+                        
+                        # Append KDE to the KDE's list
+                        KDEs[i][combo].append(kde)
+                    
         # Return the univariate and multivariate KDEs
         return KDEs, joint_KDEs
     
@@ -171,7 +169,7 @@ class checkFragility:
     def evalBayes(self, KDEs, joint_KDEs):
         
         # Initialize empty lists
-        posterior_KDEs = [[] for _ in range(len(self.D))]
+        posterior_KDEs = [{} for _ in range(len(self.D))]
         
         # Specify number of points in each dimension of evaluation grids
         # (Free to adjust this!)
@@ -180,33 +178,48 @@ class checkFragility:
         # Loop through each discipline
         for i in range(0, len(self.D)):
             
-            # Determine the number of dimensions of the joint KDEs
-            num_dimensions = np.array(joint_KDEs[i][0].data).shape[1]
-            
-            # Create the range lists
-            # (MAY WANT TO ADJUST THE RANGE OF FAILURE AMOUNT TO BE 0 TO MAX+0.5!)
-            ranges = [np.linspace(0, 1, grid_res) for _ in range(num_dimensions)]
-            
-            # Loop through each KDE
-            for j, joint_kde in enumerate(joint_KDEs[i]):
+            # Loop through each variable combination
+            for combo in joint_KDEs[i]:
                 
-                # Create an evaluation grid
-                grid = np.meshgrid(*ranges)
+                # Add combo to posterior KDE dictionaries
+                posterior_KDEs[i].setdefault(combo, [])
                 
-                # Convert grid points
-                grid_points = np.vstack([g.ravel() for g in grid]).T
+                # Determine the number of dimensions of the joint KDE
+                num_dimensions = np.array(joint_KDEs[i][combo][0].data).shape[1]
                 
-                # Evaluate the joint KDE P(A, B)
-                joint_values = joint_kde.pdf(grid_points)
+                # Initialize an empty dictionary of marginal KDEs
+                marg_kdes = {}
                 
-                # Evaluate the marginal KDEs P(B)
-                marginal_values = np.prod([kde_list[j].evaluate(grid_points[:, ind]) for ind, kde_list in enumerate(KDEs[i].values())], axis=0)
+                # Gather the relevant individual KDEs
+                for key in KDEs[i]:
+                    if key[0] in combo:
+                        marg_kdes[key] = KDEs[i][key]
+
+                # Create the range lists
+                # (MAY WANT TO ADJUST THE RANGE OF FAILURE AMOUNT TO BE 0 TO MAX+0.5!)
+                ranges = [np.linspace(0, 1, grid_res) for _ in range(num_dimensions)]
                 
-                # Calculate the posterior P(A|B)
-                posterior_values = joint_values / marginal_values
-                
-                # Store the posterior values
-                posterior_KDEs[i].append(posterior_values.reshape(*[grid_res]*num_dimensions))
+                # Loop through each joint KDE
+                for j, joint_kde in enumerate(joint_KDEs[i][combo]):
+                    
+                    # Create an evaluation grid
+                    grid = np.meshgrid(*ranges)
+                    
+                    # Convert grid points
+                    grid_points = np.vstack([g.ravel() for g in grid]).T
+                    
+                    # Evaluate the joint KDE P(A, B)
+                    joint_values = joint_kde.pdf(grid_points)
+                    
+                    # Evaluate the product of the relevant marginal KDEs
+                    marginal_values = [kde_list[j].evaluate(grid_points[:, ind]) for ind, kde_list in enumerate(marg_kdes.values())]
+                    marginal_values = np.prod(marginal_values, axis=0)
+                    
+                    # Calculate the posterior P(A|B)
+                    posterior_values = joint_values / marginal_values
+                    
+                    # Store the posterior values
+                    posterior_KDEs[i][combo].append(posterior_values.reshape(*[grid_res]*num_dimensions))
                 
         # Return the posterior KDEs
         return posterior_KDEs
