f126119b617e9e09957c358558609dfa7847a4a8
joeyvan20 <joeyvan@umich.edu>
2024-09-11 11:27:33 -0400

    Working through last unit tests

--- Python Code/Utest_connect_perceptions.py
+++ Python Code/Utest_connect_perceptions.py
@@ -12,6 +12,8 @@ LIBRARIES
 """
 from connect_perceptions import organizeData, prepareData, connectPerceptions
 import unittest
+import sympy as sp
+import numpy as np
 
 
 """
@@ -24,10 +26,36 @@ class test_connect_perceptions(unittest.TestCase):
         Initialize variables and object call for functions
         """
         
+        # Initialize sympy design variables
+        self.x = sp.symbols('x1:7')
+        
+        # Initialize tested and space remaining data for multiple disciplines
+        self.Discips = [
+            {'tested_ins': np.ones((10, 3)),
+             'ins': [self.x[0], self.x[1], self.x[2]],
+             'space_remaining': np.full((20, 3), 0.95),
+             'Fail_Amount': np.full((10,), 0.45),
+             'Pass_Amount': np.zeros(10)},
+            {'tested_ins': np.full((8, 3), 0.5),
+             'ins': [self.x[2], self.x[3], self.x[4]],
+             'space_remaining': np.full((30, 3), 0.85),
+             'Fail_Amount': np.full((8,), 0.35),
+             'Pass_Amount': np.zeros(8)},
+            {'tested_ins': np.zeros((5, 3)),
+             'ins': [self.x[0], self.x[4], self.x[5]],
+             'space_remaining': np.full((15, 3), 0.75),
+             'Fail_Amount': np.full((5,), 0.25),
+             'Pass_Amount': np.zeros(5)}
+        ]
+        
+        
     def test_organize_data(self):
         """
         Unit tests for the organizeData function
         """
+        
+        # Execute function
+        x_full, y_full, x_vars = organizeData(self.Discips)
     
     
     def test_prepare_data(self):
