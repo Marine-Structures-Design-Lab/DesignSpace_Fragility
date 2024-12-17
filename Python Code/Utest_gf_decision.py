e59c510717f714b2b30d70571eab8fa8045a0a92
joeyvan20 <joeyvan@umich.edu>
2024-12-17 09:24:43 -0500

    Completed gf_decision unit tests

--- Python Code/Utest_gf_decision.py
+++ Python Code/Utest_gf_decision.py
@@ -18,23 +18,73 @@ import unittest
 CLASSES
 """
 class test_gf_decision(unittest.TestCase):
-
-    def setUp(self):
-        """
-        Initialize variables and object call for methods
-        """
-        
     
     def test_fixed(self):
         """
         Unit tests for the Fixed method
         """
         
+        # Create gradient factor decision coefficients
+        gf_decide = {
+            "strategy": "Fixed",
+            "coefficients": [0.3]
+        }
+        
+        # Set different gradient factor values
+        gradient_factor1 = 0.31
+        gradient_factor2 = 0.29
+        
+        # Set current and maximum time iterations
+        iters = 100
+        iters_max = 200
+        
+        # Create object calls
+        gradfact1 = gfDecider(gf_decide['coefficients'], 
+                              gradient_factor1, iters, iters_max)
+        gradfact2 = gfDecider(gf_decide['coefficients'],
+                              gradient_factor2, iters, iters_max)
+        
+        # Call gradient factor decision strategy
+        break_loop1 = getattr(gradfact1, gf_decide['strategy'])()
+        break_loop2 = getattr(gradfact2, gf_decide['strategy'])()
+        
+        # Check whether gf decision strategy results meet expected results
+        self.assertTrue(break_loop1)
+        self.assertFalse(break_loop2)
+        
     
     def test_linear(self):
         """
         Unit tests for the Linear method
         """
+        
+        # Create gradient factor decision coefficients
+        gf_decide = {
+            "strategy": "Linear",
+            "coefficients": [-0.006, 0.6]
+        }
+        
+        # Set different gradient factor values
+        gradient_factor1 = 0.31
+        gradient_factor2 = 0.29
+        
+        # Set current and maximum time iterations
+        iters = 100
+        iters_max = 200
+        
+        # Create object calls
+        gradfact1 = gfDecider(gf_decide['coefficients'], 
+                              gradient_factor1, iters, iters_max)
+        gradfact2 = gfDecider(gf_decide['coefficients'],
+                              gradient_factor2, iters, iters_max)
+        
+        # Call gradient factor decision strategy
+        break_loop1 = getattr(gradfact1, gf_decide['strategy'])()
+        break_loop2 = getattr(gradfact2, gf_decide['strategy'])()
+        
+        # Check whether gf decision strategy results meet expected results
+        self.assertTrue(break_loop1)
+        self.assertFalse(break_loop2)
     
     
     def test_quadratic(self):
@@ -42,10 +92,35 @@ class test_gf_decision(unittest.TestCase):
         Unit tests for the Quadratic method
         """
         
+        # Create gradient factor decision coefficients
+        gf_decide = {
+            "strategy": "Quadratic",
+            "coefficients": [0.00009, 100, 0]
+        }
+        
+        # Set different gradient factor values
+        gradient_factor1 = 0.00226
+        gradient_factor2 = 0.00224
+        
+        # Set current and maximum time iterations
+        iters = 190
+        iters_max = 200
+        
+        # Create object calls
+        gradfact1 = gfDecider(gf_decide['coefficients'], 
+                              gradient_factor1, iters, iters_max)
+        gradfact2 = gfDecider(gf_decide['coefficients'],
+                              gradient_factor2, iters, iters_max)
+        
+        # Call gradient factor decision strategy
+        break_loop1 = getattr(gradfact1, gf_decide['strategy'])()
+        break_loop2 = getattr(gradfact2, gf_decide['strategy'])()
+        
+        # Check whether gf decision strategy results meet expected results
+        self.assertTrue(break_loop1)
+        self.assertFalse(break_loop2)
         
         
-
-
 """
 SCRIPT
 """
--- Python Code/gf_decision.py
+++ Python Code/gf_decision.py
@@ -1,6 +1,8 @@
 """
 SUMMARY:
-
+Makes the decision on whether the gathered gradient factor value is high enough
+to implement a space reduction decision based on the chosen user decision
+strategy.
 
 CREATOR:
 Joseph B. Van Houten
@@ -11,18 +13,36 @@ joeyvan@umich.edu
 LIBRARIES
 """
 
+
 """
 FUNCTION
 """
 def thresholdCheck(gf, threshold):
+    """
+    Description
+    -----------
+    Determines whether the gradient factor value exceeds the decision 
+    threshold.
+
+    Parameters
+    ----------
+    gf : Float
+        Gathered gradient factor value of the space reduction proposed
+    threshold : Float
+        Minimum gradient factor value for which it is okay to implement a space
+        reduction
+
+    Returns
+    -------
+    True or False
+        Whether it is okay to implement a space reduction (True) or not (False)
+    """
     
     if gf >= threshold:
         return True
     else:
         return False
     
-    
-
 
 """
 CLASS
@@ -34,28 +54,81 @@ class gfDecider:
         self.gf = gradient_factor
         self.it = iters
         self.itm = iters_max
+        """
+        Parameters
+        ----------
+        coefficients : List of floats
+            The chosen user inputs that descirbe the equation used for setting
+            the threshold of gradient factor decision strategy
+        gradient_factor : Float
+            Gathered gradient factor value of the space reduction proposed
+        iters : Integer
+            Current time iteration of the SBD simulation
+        iters_max : Integer
+            Total amount of time iterations allotted to the SBD simulation
+        """
         return
     
     
     def Fixed(self):
+        """
+        Description
+        -----------
+        Sets a fixed value for the minimum gradient factor threshold.
+
+        Returns
+        -------
+        True or False
+            Whether it is okay to implement a space reduction (True) or not 
+            (False)
+        """
         
+        # Set the fixed value of the threshold
         threshold = self.coeff[0]
         
+        # Check whether the gradient factor exceeds the threshold
         return thresholdCheck(self.gf, threshold)
     
     
     def Linear(self):
+        """
+        Description
+        -----------
+        Sets a linear equation for the minimum gradient factor threshold.
+
+        Returns
+        -------
+        True or False
+            Whether it is okay to implement a space reduction (True) or not 
+            (False)
+        """
         
-        threshold = self.coeff[0]*(float(self.it)/float(self.itm)*100) + self.coeff[1]
+        # Set the linear equation for the threshold
+        threshold = self.coeff[0]*(float(self.it)/float(self.itm)*100) \
+            + self.coeff[1]
         
+        # Check whether the gradient factor exceeds the threshold
         return thresholdCheck(self.gf, threshold)
 
     
     def Quadratic(self):
+        """
+        Description
+        -----------
+        Sets a quadratic equation for the minimum gradient factor threshold.
+
+        Returns
+        -------
+        True or False
+            Whether it is okay to implement a space reduction (True) or not 
+            (False)
+        """
         
-        threshold = self.coeff[0]*((float(self.it)/float(self.itm)*100) - self.coeff[1])**2 - self.coeff[2]
+        # Set the quadratic equation for the threshold
+        threshold = self.coeff[0]*((float(self.it)/float(self.itm)*100) \
+                                   - self.coeff[1])**2 - self.coeff[2]
         
+        # Check whether the gradient factor exceeds the threshold
         return thresholdCheck(self.gf, threshold)
-    
     
     
