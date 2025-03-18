630c2faddf8384280b54616397532f65cee64d1b
joeyvan20 <joeyvan@umich.edu>
2025-03-17 19:45:41 -0700

    Updated post-processing

--- Python Code/Simulation_Results_Linear_50/read_data2.py
+++ Python Code/Simulation_Results_Linear_50/read_data2.py
@@ -26,22 +26,6 @@ READ DATA
 # Save the current directory's path
 original_dir = os.getcwd()
 
-# # Read in the data from Test Case 2
-# os.chdir('./Test Case 2/Gradient_Factor')
-# with open('load_data.py') as file:
-#     exec(file.read())
-
-# # Change back to the original directory
-# os.chdir(original_dir)
-
-# # Read in the data from Test Case 3
-# os.chdir('./Test Case 3/Gradient_Factor')
-# with open('load_data.py') as file:
-#     exec(file.read())
-
-# # Change back to the original directory
-# os.chdir(original_dir)
-
 # Read in the data from Test Case 4
 os.chdir('./Test Case 4/Gradient_Factor')
 with open('load_data.py') as file:
@@ -62,12 +46,6 @@ os.chdir(original_dir)
 """
 SAVE DATA
 """
-
-# with open('Gradient_Factor_2.pkl', 'wb') as f:
-#     pickle.dump(Gradient_Factor_2, f)
-
-# with open('Gradient_Factor_3.pkl', 'wb') as f:
-#     pickle.dump(Gradient_Factor_3, f)
 
 with open('Gradient_Factor_4.pkl', 'wb') as f:
     pickle.dump(Gradient_Factor_4, f)
